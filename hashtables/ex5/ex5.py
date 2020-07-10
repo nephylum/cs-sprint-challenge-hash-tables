25447dd7b5215dff0ed48cfdc2b8a518fd90e41d
nephylum <25045705+nephylum@users.noreply.github.com>
2020-07-10 09:35:28 -0700

    working

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -1,13 +1,32 @@
-# Your code here
-
-
+import os
 
 def finder(files, queries):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
 
+    # will work but is o(n^2) time complex
+    # result = []
+    # for item in files:
+    #     for q in queries:
+    #         if q in item:
+    #             result.append(item)
+
+    #get files from paths, set file as key and path as value in cache
+    cache = {}
+    result = []
+    for path in files:
+        filename = os.path.basename(path)
+        if filename not in cache:
+            cache[filename] = [path]
+        elif filename in cache:
+            cache[filename].append(path)
+
+    #check if query is in cache, if so append value to result
+    for q in queries:
+        if q in cache:
+            if len(cache[q]) == 1:
+                result.append(cache[q][0])
+            elif len(cache[q]) > 1:
+                for x in cache[q]:
+                    result.append(x)
     return result
 
 
