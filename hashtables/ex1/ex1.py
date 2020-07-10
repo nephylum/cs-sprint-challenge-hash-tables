3f1754367bb8064c848acbbe9e74b2c2b8858817
nephylum <25045705+nephylum@users.noreply.github.com>
2020-07-10 08:17:09 -0700

    working

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -2,6 +2,20 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    hash = {}
+    for x in range(length):
+    #check if already added to hash
+        if weights[x] in hash:
+        #check if the key and value add to goal, if so return
+            if limit-weights[x] in hash:
+                print(hash[weights[x]], x)
+                return x, hash[weights[x]]
+        hash[weights[x]] = x
+    #check every item in hash
+    for x in hash:
+        weight = x
+        to_limit = limit - weight
+        if to_limit in hash:
+            return hash[to_limit], hash[weight]
 
     return None
